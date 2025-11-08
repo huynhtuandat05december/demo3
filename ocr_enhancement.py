3648676a07f11c0126982a028fde6b668a294e2f
huynhtuandat05december <huynhtuandat05december@gmail.com>
2025-11-08 16:59:01 +0700

    first commit

--- ocr_enhancement.py
+++ ocr_enhancement.py
@@ -24,6 +24,7 @@ class SignTextExtractor:
 
         try:
             self.ocr = PaddleOCR(
+                lang='vi',
                 use_doc_orientation_classify=False,
                 use_doc_unwarping=False,
                 use_textline_orientation=False
